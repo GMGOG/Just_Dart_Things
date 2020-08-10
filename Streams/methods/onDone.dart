52d5c6ab5f514b6cdca47d3a968f35dac5768b7c
Geetansh Monga <gmgog0402@gmail.com>
2020-08-10 16:59:19 +0530

    on Done

---
+++ Streams/methods/onDone.dart
@@ -0,0 +1,20 @@
+import 'dart:async';
+
+Stream<int> streamExample() async* {
+  for (int i = 0; i < 10; i++) {
+    yield i;
+    await Future.delayed(Duration(seconds: 1));
+  }
+}
+
+void main() {
+  streamExample().listen(
+    (data) {
+      print(data);
+    },
+  ).onDone(
+    () {
+      print("Finished");
+    },
+  );
+}
